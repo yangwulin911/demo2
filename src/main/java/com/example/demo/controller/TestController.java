0134bdbcf8b71533535328c59c858d5df3e6b11b
ywl <yangwulin911@163.com>
2019-08-30 10:58:23 +0800

    demo

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author admin
  */
-@Controller
+@RestController
 public class TestController {
 
     @RequestMapping("/test")
